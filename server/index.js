8180cb402febbcdb38854d1a31dae7996c27f1fb
Connor Barry <cbarry578@gmail.com>
2021-12-16 13:44:55 -0800

    testing

--- server/index.js
+++ server/index.js
@@ -4,6 +4,15 @@ const app = express()
 
 app.use(express.json())
 
+// include and initialize the rollbar library with your access token
+const Rollbar = require("rollbar");
+const rollbar = new Rollbar({
+  accessToken: '38fca8ee95af4a529bd13b09092498a6',
+  captureUncaught: true,
+  captureUnhandledRejections: true
+});
+
+let students = []
 
 app.get('/', function (req,res) {
     res.sendFile(path.join(__dirname, '../public/index.html'))
@@ -17,6 +26,17 @@ app.get('/css', (req,res) => {
     res.sendFile(path.join(__dirname, '../public/styles.css'))
 })
 
+app.post('/api/student', (req,res) => {
+    let {name} = req.body
+    name = name.trim()
 
+    students.push(name)
+    rollbar.log('Student added successfully', {author: 'Connor', type: 'manual'})
+
+    res.status(200).send(students)
+})
+
+
+app.use(rollbar.errorHandler())
 const port = process.env.PORT || 9505
 app.listen(port, () => console.log(`We are up and running on port ${port}`))
